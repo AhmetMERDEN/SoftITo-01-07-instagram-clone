7dac70c6dbc7de23c1e9ae13cd985fe6a8c18b04
AhmetMERDEN <Kamusari_Kaiser@hotmail.com>
2023-03-16 10:18:03 +0300

    project updated

--- lib/components/bottom_navbar.dart
+++ lib/components/bottom_navbar.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:instagram_clone/view_models/home_page_view_model.dart';
+import 'package:provider/provider.dart';
 
-class CustomBottomNavBar extends StatelessWidget {
+class CustomBottomNavBar extends StatefulWidget {
   const CustomBottomNavBar({super.key});
 
+  @override
+  State<CustomBottomNavBar> createState() => _CustomBottomNavBarState();
+}
+
+class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -15,43 +20,31 @@ class CustomBottomNavBar extends StatelessWidget {
         ),
         child:
             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          GestureDetector(
-            child: Image.asset(
-              "assets/icons/home.png",
-              width: 28,
-              height: 28,
-            ),
-          ),
-          GestureDetector(
-            child: Image.asset(
-              "assets/icons/search.png",
-              width: 28,
-              height: 28,
-            ),
-          ),
-          GestureDetector(
-            child: Image.asset(
-              "assets/icons/add.png",
-              width: 28,
-              height: 28,
-            ),
-          ),
-          GestureDetector(
-            child: Image.asset(
-              "assets/icons/reels.png",
-              width: 28,
-              height: 28,
-            ),
-          ),
-          GestureDetector(
-            child: Image.asset(
-              "assets/icons/profil.png",
-              width: 28,
-              height: 28,
-            ),
-          ),
+          for (int i = 0; i < icons.length; i++) iconButton(icons[i], i),
         ]),
       ),
     );
   }
+
+  List<String> icons = [
+    "assets/icons/home.png",
+    "assets/icons/search.png",
+    "assets/icons/add.png",
+    "assets/icons/reels.png",
+    "assets/icons/profil.png",
+  ];
+
+  Widget iconButton(String icon, int index) => GestureDetector(
+        onTap: () {
+          context.read<HomePageViewModel>().setPage(index);
+        },
+        child: Opacity(
+          opacity: context.watch<HomePageViewModel>().page == index ? 1.0 : 0.7,
+          child: Image.asset(
+            icon,
+            width: 28,
+            height: 28,
+          ),
+        ),
+      );
 }
